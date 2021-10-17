903a121d413491d70e2f97c119af96a0310c6d54
Aditya Shahapurkar <shahapurkaraditya@gmail.com>
2021-10-17 10:13:56 +0530

    Test0

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -5,5 +5,14 @@ class StringCalculator {
     public int add(String input) {
         return 0;
     }
-
+    @Test
+    public void testEmptyString() {
+      assertEquals(0, Calculator.add(""));
+    }
+    
+    
+    
+    
+    
+    
 }
