2dd5f34de34e059c6247adff008baaeeda8a0ccd
Aditya Shahapurkar <shahapurkaraditya@gmail.com>
2021-10-17 11:09:32 +0530

    Test3

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -11,17 +11,29 @@ class StringCalculator {
       	    String[] numbers = input.split(",");
       	    return toInt(numbers[0]) + toInt(numbers[1]);
       	  }
+    	  else if(input.contains(",")){
+    		  String[] numbers = input.split(",");
+    		  int total = 0;
+    		  for(String number : numbers){
+    		    total += toInt(number);
+    		}
+    		  return total;
+    		}
     	  else
     	  	return 1;
     	}
-  
-
-    	private static int toInt(String number){
-    	  return Integer.parseInt(number);
+    private static int toInt(String number){
+  	  return Integer.parseInt(number);
+  	}
+    private static String[] splitNumbers(String numbers){
+    	  return numbers.split(",");
     	}
-    
-    
-    
-    
-    
+
+    	private static int sum(String[] numbers){
+    	  int total = 0;
+    	  for(String number : numbers){
+    	    total += toInt(number);
+    	  }
+    	  return total;
+    	}
 }
