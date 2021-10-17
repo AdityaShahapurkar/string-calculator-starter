da24b63170777a5dfb91c9fdac34dfb3546d268d
Aditya Shahapurkar <shahapurkaraditya@gmail.com>
2021-10-17 10:36:18 +0530

    Test1

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,13 +2,14 @@ package calculator;
 
 class StringCalculator {
 
-    public int add(String input) {
-        return 0;
-    }
-    @Test
-    public void testEmptyString() {
-      assertEquals(0, Calculator.add(""));
-    }
+ 
+    public static int add(String input){
+    	  if(input.equals("")){
+    	    return 0;
+    	  }
+    	  else
+    	  	return 1;
+    	}
     
     
     
