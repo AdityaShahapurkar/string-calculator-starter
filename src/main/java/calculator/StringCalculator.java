0811c7f9703b5cc2fcbf30c801643d3448d57731
Aditya Shahapurkar <shahapurkaraditya@gmail.com>
2021-10-17 10:41:29 +0530

    Test2

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -7,10 +7,18 @@ class StringCalculator {
     	  if(input.equals("")){
     	    return 0;
     	  }
+    	  else if(input.contains(",")){
+      	    String[] numbers = input.split(",");
+      	    return toInt(numbers[0]) + toInt(numbers[1]);
+      	  }
     	  else
     	  	return 1;
     	}
-    
+  
+
+    	private static int toInt(String number){
+    	  return Integer.parseInt(number);
+    	}
     
     
     
